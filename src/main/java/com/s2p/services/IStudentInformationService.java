27b3d99720fcf9c819e8179eb548bcacf74db7bc
Kanika Agrawal <kn2269@srmist.edu.in>
2025-10-01 12:12:04 +0530

    junit test file

--- src/main/java/com/s2p/services/IStudentInformationService.java
+++ src/main/java/com/s2p/services/IStudentInformationService.java
@@ -1,10 +1,8 @@
 package com.s2p.services;
 
 import com.s2p.dto.StudentInformationDto;
-import org.apache.coyote.BadRequestException;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IStudentInformationService {
 
@@ -12,24 +10,9 @@ public interface IStudentInformationService {
 
     StudentInformationDto updateStudentInformationByEmail(String email, StudentInformationDto dto);
 
-    StudentInformationDto createStudentInformation(StudentInformationDto studentInformationDto) throws BadRequestException;
-
-    String deleteStudentByEmail(String email);
-
     String deleteStudent(String email);
 
-    Optional<StudentInformationDto> getStudentByEmail(String email);
+    StudentInformationDto getStudentByEmail(String email);
 
     List<StudentInformationDto> getAllStudents();
-
-    public StudentInformationDto updateStudentByEmail(String email, StudentInformationDto studentInformationDto);
-    public List<StudentInformationDto> searchStudents(
-            String firstName,
-            String lastName,
-            String email,
-            String collegeName,
-            String degreeName,
-            String semester,
-            String passingYear,
-            Boolean isGraduated);
 }
